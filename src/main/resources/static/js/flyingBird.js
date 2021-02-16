f0d3944748218a3f64a4950151d1186cb335c451
kfiddle <kenjfiddle@gmail.com>
2021-02-15 23:14:12 -0500

    more bird taking off

--- src/main/resources/static/js/flyingBird.js
+++ src/main/resources/static/js/flyingBird.js
@@ -1,25 +1,29 @@
-
-
 const bird = document.getElementById("bird");
 
 
-setTimeout(flyBird, 3000);
+setTimeout(openWings, 2000);
+setTimeout(flapWings, 3000);
 
-function flyBird() {
+function openWings() {
 
-    for (let i = 2; i < 6; i++) {
-        setTimeout(setBird, 40*i, i);
+    for (let i = 2; i < 9; i++) {
+        setTimeout(setBird, 90 * i, i);
     }
 
+}
 
-    // setTimeout(printWords, i*100, words[i]);
-
-
+function flapWings() {
+    let flaps = [7, 6, 5, 6, 7, 6, 5, 6, 7, 6, 5, 6, 7, 6, 5, 6, 7];
+    for (let i = 0; i < flaps.length; i++) {
+        setTimeout(setBird, 80 * i, flaps[i]);
+    }
 
 }
+
 
 function setBird(i) {
     bird.setAttribute("src", "/images/bird" + i + ".png");
 
 }
 
+// setTimeout(printWords, i*100, words[i]);
