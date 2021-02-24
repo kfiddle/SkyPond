b0df52b9d792865231e09088d8a6a2404669185d
kfiddle <kenjfiddle@gmail.com>
2021-02-23 21:37:19 -0500

    more for mobile

--- src/main/resources/static/homePageMobileView.js
+++ src/main/resources/static/homePageMobileView.js
@@ -1,1 +1,11 @@
-console.log('yes');
+const body = document.querySelector('body');
+const logoContainer = document.getElementById('homePage');
+
+let mobileViewVerify = window.matchMedia('(max-width:700px');
+
+
+if (mobileViewVerify.matches === true) {
+    body.style.marginTop = "100%";
+    logoContainer.classList.remove('homePage');
+    logoContainer.classList.add('homePageMobileOnly');
+}
