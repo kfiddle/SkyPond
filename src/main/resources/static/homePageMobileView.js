790f963d1043cfc80fa0a543ec313f77fbe47c4a
kfiddle <kenjfiddle@gmail.com>
2021-02-26 13:43:41 -0500

    more stuff for media

--- src/main/resources/static/homePageMobileView.js
+++ src/main/resources/static/homePageMobileView.js
@@ -1,11 +1,15 @@
 const body = document.querySelector('body');
 const logoContainer = document.getElementById('homePage');
+const logo =  document.getElementById('skyPondLogo');
+const title =  document.getElementById('homePage_title');
 
 let mobileViewVerify = window.matchMedia('(max-width:700px');
 
 
 if (mobileViewVerify.matches === true) {
-    body.style.marginTop = "100%";
+    body.style.marginTop = "80%";
     logoContainer.classList.remove('homePage');
     logoContainer.classList.add('homePageMobileOnly');
+    logo.style.width = "36vw";
+    title.style.fontSize = "9vw";
 }
