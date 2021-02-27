f837a08335d2b52db1e1f9c65f49344a866ec831
kfiddle <kenjfiddle@gmail.com>
2021-02-27 12:49:59 -0500

    media queries in js

--- src/main/resources/static/homePageMobileView.js
+++ src/main/resources/static/homePageMobileView.js
@@ -4,7 +4,8 @@ const logo = document.getElementById('skyPondLogo');
 const title = document.getElementById('homePage_title');
 
 let phoneScreen = window.matchMedia('(max-width:700px');
-
+let hugeScreen = window.matchMedia('(min-width: 1598px');
+let mediumScreen = window.matchMedia('(max-width: 795px)');
 
 if (phoneScreen.matches) {
     body.style.marginTop = "40%";
@@ -14,6 +15,15 @@ if (phoneScreen.matches) {
     title.style.fontSize = "9vw";
 }
 
+if (hugeScreen.matches) {
+    title.style.fontSize = "5rem";
+}
+
+if (mediumScreen.matches) {
+    body.style.marginTop = "40%";
+    logoContainer.classList.remove('homePage');
+    logoContainer.classList.add('homePageMobileOnly');
+}
 
 
 
