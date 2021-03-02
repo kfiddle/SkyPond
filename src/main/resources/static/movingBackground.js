845397bc7a1656ef4fd0b54489153e4be743d119
kfiddle <kenjfiddle@gmail.com>
2021-03-02 16:17:43 -0500

    work on transition from home to contacts

--- src/main/resources/static/movingBackground.js
+++ src/main/resources/static/movingBackground.js
@@ -1,8 +1,7 @@
 const background = document.getElementById('bgImage');
 const tint = document.getElementById('tint');
 const hoverLogo = document.getElementById('homePage');
-
-let hovered = false;
+const header = document.getElementById('header');
 
 
 const darkenTint = (i) => {
@@ -13,40 +12,30 @@ const positionBackground = (i) => {
     background.style.backgroundPositionY = i + "%";
 }
 
+const showHeader = () => {
+    header.classList.remove('hidden');
+}
+
 const scrollBackground = () => {
     for (let i = 1; i < 120; i++) {
         setTimeout(positionBackground, i * 15, i);
     }
 }
 
-hoverLogo.addEventListener('mouseover', () => {
+const switchToContactPage = () => {
+    window.location = "/contact";
+}
+
+hoverLogo.addEventListener('click', () => {
     background.style.position = "relative";
     background.style.backgroundRepeat = "no-repeat";
 
     for (let i = 1; i < 49; i++) {
         setTimeout(darkenTint, i * 90, (49 - i));
     }
+
     setTimeout(scrollBackground, 2000);
+    setTimeout(showHeader, 3300);
+    setTimeout(switchToContactPage, 3600);
 
-})
-
-
-// const setImage = (i) => {
-//     background.style.height = i + "%";
-// }
-//
-//
-//
-// hoverLogo.addEventListener('mouseover', ()=> {
-//
-//     if (hovered === false) {
-//         background.classList.remove('bgImage');
-//         background.classList.add('bgImageAfterLoading');
-//
-//         for (let i = 100; i > 15; i--) {
-//             setTimeout(setImage, (100 - i) * 20, i);
-//         }
-//         background.style.height = "40%";
-//     }
-//     hovered = true;
-// })
+});
