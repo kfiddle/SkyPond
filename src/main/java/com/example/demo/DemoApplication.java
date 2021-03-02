0908079b47fb3e10e651105274a593d43c4aa851
kfiddle <kenjfiddle@gmail.com>
2021-03-02 13:52:57 -0500

    forget the /home now

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.ArrayList;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,6 +15,9 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+
+
 
 }
 
