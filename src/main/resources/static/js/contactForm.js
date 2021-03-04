a2a673d0028d6f8bfd154fc041ab8979a085c486
kfiddle <kenjfiddle@gmail.com>
2021-03-04 17:49:24 -0500

    beginning of working phone formatter

--- src/main/resources/static/js/contactForm.js
+++ src/main/resources/static/js/contactForm.js
@@ -1,11 +1,49 @@
 const button = document.getElementById('button');
+const phone = document.getElementById('phone');
+
+
+const formatPhoneNumber = (event) => {
+    let displayedNumber = phone.value;
+    let key = event.keyCode;
+
+    if (key === 8) {
+        displayedNumber = displayedNumber.substring(-1);
+        phone.value = displayedNumber;
+
+    } else if ((key > 47 && key < 58) || (key > 96 && key < 105)) {
+
+        if (displayedNumber.length === 3) {
+            displayedNumber = '(' + displayedNumber + ') ';
+            phone.value = displayedNumber;
+        }
+
+        if (displayedNumber.length === 9) {
+            if (event.keyCode === 189) {
+                phone.value = displayedNumber;
+            } else {
+                phone.value = displayedNumber + '-';
+            }
+        }
+
+        if (displayedNumber.length === 14) {
+            displayedNumber = displayedNumber.substring(0, 13);
+            phone.value = displayedNumber;
+        }
+    }
+    else {
+        displayedNumber = displayedNumber.substring(0, displayedNumber.length - 1);
+        phone.value = displayedNumber;
+
+}
+    console.log(displayedNumber);
+
+}
 
 
 const submitCustomer = () => {
 
     let fullName = document.getElementById('name').value;
     let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
 
 
     let names = fullName.split(" ");
@@ -13,10 +51,10 @@ const submitCustomer = () => {
     let lastName = names[1];
 
     let formData = {
-        firstName : firstName,
-        lastName : lastName,
-        phoneNumber : phone,
-        email : email,
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phone,
+        email: email,
     }
 
     $.ajax({
@@ -29,9 +67,42 @@ const submitCustomer = () => {
     )
 
 
+}
 
+
+phone.addEventListener('keydown', () => {
+    formatPhoneNumber(event);
+});
+button.addEventListener('click', submitCustomer);
+
+/*
+the "sort of" phone formatters below. They don't work well enough as is.
+
+const formatPhoneNumber = (value) => {
+    let displayedNumber = value.replace(/[^\d]/g,"");
+
+    let phoneNumberLength = value.length;
+
+    if (phoneNumberLength < 4) {
+        phone.value = displayedNumber;
+    }
+
+    if (phoneNumberLength > 4 && phoneNumberLength < 7) {
+        phone.value = `(${displayedNumber.slice(0, 3)}) ${displayedNumber.slice(3)}`;
+    }
+
+    phone.value = `(${displayedNumber.slice(0, 3)}) ${displayedNumber.slice(3, 6)}-${displayedNumber.slice(6, 9)}`;
 
 }
 
 
-button.addEventListener('click', submitCustomer);
+
+ let cleaned = ('' + value).replace(/\D/g, '');
+
+    let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+    if (match) {
+        phone.value = '(' + match[1] + ')' + match[2] + '-' + match[3];
+    }
+
+ */
