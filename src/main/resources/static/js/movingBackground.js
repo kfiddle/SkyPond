47734c654467c8e10c93b037f6c0ab2b8f5a616c
kfiddle <kenjfiddle@gmail.com>
2021-03-10 13:39:44 -0500

    merge fixes

--- src/main/resources/static/js/movingBackground.js
+++ src/main/resources/static/js/movingBackground.js
@@ -1,24 +1,16 @@
 const background = document.getElementById('bgImage');
-const tint = document.getElementById('tint');
 const hoverLogo = document.getElementById('homePage');
 const header = document.getElementById('header');
-
-
-const darkenTint = (i) => {
-    tint.style.opacity = (i * 0.01).toString();
-}
 
 const positionBackground = (i) => {
     background.style.backgroundPositionY = i + "%";
 }
 
-const showHeader = () => {
-    header.classList.remove('hidden');
-}
 
 const scrollBackground = () => {
-    for (let i = 1; i < 120; i++) {
-        setTimeout(positionBackground, i * 15, i);
+    for (let i = 1; i < 105; i++) {
+        setTimeout(positionBackground, i * 10, i);
+
     }
 }
 
@@ -30,12 +22,9 @@ hoverLogo.addEventListener('click', () => {
     background.style.position = "relative";
     background.style.backgroundRepeat = "no-repeat";
 
-    for (let i = 1; i < 49; i++) {
-        setTimeout(darkenTint, i * 90, (49 - i));
-    }
 
-    setTimeout(scrollBackground, 2000);
-    setTimeout(showHeader, 3300);
-    setTimeout(switchToContactPage, 3600);
+    setTimeout(scrollBackground, 1000);
+    setTimeout(switchToContactPage, 2000);
+
 
 });
