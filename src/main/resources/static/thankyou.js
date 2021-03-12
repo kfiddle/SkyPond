640da7fdafa213eca6dfd0d304d818ca7cbe370f
ksamoviski <ksamoviski@gmail.com>
2021-03-12 12:11:09 -0500

    playing with thank you modal

---
+++ src/main/resources/static/thankyou.js
@@ -0,0 +1,20 @@
+// Modal can be opened
+function addModalEventListeners() {
+
+    const modalTriggerElement = document.querySelector('#modal-trigger');
+
+    const modalTriggerClickHandler = function() {
+        document.body.classList.add('modal-open');
+    };
+
+    modalTriggerElement.addEventListener('click', modalTriggerClickHandler);
+
+    // Modal can be closed
+    const modalCloseButton = document.querySelector('#modal button');
+
+    const modalCloseButtonClickHandler = function() {
+        document.body.classList.remove('modal-open');
+    };
+
+    modalCloseButton.addEventListener('click', modalCloseButtonClickHandler);
+}
