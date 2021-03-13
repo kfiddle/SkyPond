d83422b1178b6a9efce159b61fb61bd14c7003f2
kfiddle <kenjfiddle@gmail.com>
2021-03-13 12:00:40 -0500

    merging flying bird js and odds and ends

---
+++ src/main/resources/static/js/flyingBird.js
@@ -0,0 +1,18 @@
+const bird = document.getElementById('bird');
+
+
+setTimeout(takeOff, 1000);
+
+
+function setBird(i) {
+    bird.setAttribute("src", "images/birdLaunchStills/bird" + i + ".png");
+}
+
+
+
+function takeOff() {
+
+    for (let i = 2; i < 6; i++) {
+        setTimeout(setBird, 150 *i, i);
+    }
+}
