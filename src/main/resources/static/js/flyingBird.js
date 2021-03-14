0544f884849bef67fa525564e4f97553cc18e062
kfiddle <kenjfiddle@gmail.com>
2021-03-13 20:32:25 -0500

    a flying bird

--- src/main/resources/static/js/flyingBird.js
+++ src/main/resources/static/js/flyingBird.js
@@ -1,18 +1,28 @@
 const bird = document.getElementById('bird');
 
-
-setTimeout(takeOff, 1000);
-
-
-function setBird(i) {
-    bird.setAttribute("src", "images/flyingBird/heron_inFlight-" + i + ".png");
+function displayBird(frame, leftSpot, bottomSpot) {
+    bird.setAttribute("src", "images/flyingBird/heron_inFlight-" + frame + ".png");
+    bird.style.left = leftSpot + "vw";
+    bird.style.bottom = bottomSpot + "vh";
 }
 
+function fly(frame, time) {
 
+    let flyingLoop = [1, 2, 3, 4, 5, 6, 7, 8,
+        9, 10, 11, 12, 9, 10, 11, 12, 9, 10, 11, 12, 9, 10, 11, 12, 9, 10, 11, 12];
 
-function takeOff() {
+    let leftSpots = [7,8,9,10,11,12,13,14,15,16,18,20,22,24,28,32,36,40,44,48,52,56,60,64];
+    let bottomSpots = [];
+    for (let i = 0; i < 37; i++) {
+        bottomSpots[i] = i + 13;
+    }
 
-    for (let i = 2; i < 13; i++) {
-        setTimeout(setBird, 150 *i, i);
+    if (frame < 23) {
+        setTimeout(displayBird, time * 150, flyingLoop[frame], leftSpots[frame], bottomSpots[frame]);
+        frame++;
+        time++;
+        fly(frame, time);
     }
 }
+
+fly(0, 1);
