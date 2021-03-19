259e5b538a6175bb8e61cd0772061ec99a29732c
kfiddle <kenjfiddle@gmail.com>
2021-03-18 21:17:27 -0400

    various repairs to js, media queries, etc...

--- src/main/resources/static/js/contactFormViews.js
+++
@@ -1,8 +0,0 @@
-const title = document.querySelector(".basicPage_title");
-
-let hugeScreen = window.matchMedia('(min-width: 1598px');
-
-
-if (hugeScreen.matches) {
-    title.style.fontSize = "4rem";
-}
--- src/main/resources/static/js/homePageMediaQueries.js
+++ src/main/resources/static/js/homePageMediaQueries.js
@@ -1,29 +1,29 @@
-const body = document.querySelector('body');
 const logoContainer = document.getElementById('homePage');
 const logo = document.getElementById('skyPondLogo');
-const title = document.getElementById('homePage_title');
-
+const homePageTitle = document.getElementById('homePage_title');
+const bodyContainer = document.querySelector("body");
 let phoneScreen = window.matchMedia('(max-width:700px');
 let hugeScreen = window.matchMedia('(min-width: 1598px');
 let smallishScreen = window.matchMedia('(max-width: 795px)');
 let mediumScreen = window.matchMedia('(min-width: 796px)');
 
 if (phoneScreen.matches) {
-    body.style.marginTop = "40%";
+    console.log("we're in da phone")
+    bodyContainer.style.marginTop = "40%";
     logoContainer.classList.remove('homePage');
     logoContainer.classList.add('homePageMobileOnly');
     logo.style.width = "36vw";
-    title.style.fontSize = "9vw";
+    homePageTitle.style.fontSize = "9vw";
 }
 
 if (hugeScreen.matches) {
-    title.style.fontSize = "5rem";
+    homePageTitle.style.fontSize = "5rem";
     logoContainer.style.marginTop = "100%";
 
 }
 
 if (smallishScreen.matches) {
-    body.style.marginTop = "20%";
+    bodyContainer.style.marginTop = "20%";
     logoContainer.classList.remove('homePage');
     logoContainer.classList.add('homePageMobileOnly');
 }
@@ -31,6 +31,18 @@ if (smallishScreen.matches) {
 if (mediumScreen.matches) {
     logoContainer.style.marginTop = "45%";
 }
+
+//
+//      ****** snippets from deleted contact form view js
+//
+// const title = document.querySelector(".basicPage_title");
+//
+// let hugeScreen = window.matchMedia('(min-width: 1598px');
+//
+//
+// if (hugeScreen.matches) {
+//     title.style.fontSize = "4rem";
+// }
 
 
 
